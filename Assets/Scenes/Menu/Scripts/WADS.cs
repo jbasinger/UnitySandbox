257d5c04704aa53f6c3917dc2d9568143256b9b4
Justin Basinger <justin.m.basinger@gmail.com>
2020-04-17 15:23:02 -0400

    First commit!

---
+++ Assets/Scenes/Menu/Scripts/WADS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WADS : MonoBehaviour
+{
+
+	[Range(0,50)]
+	public float speed = 5f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float delta = speed * Time.deltaTime;
+		transform.position += new Vector3(Input.GetAxis("Horizontal") * delta, Input.GetAxis("Vertical") * delta);
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# The Sandbox
+
+This project is a sandbox to try stupid things and to setup a DevOps pipeline to http://itch.io for builds.
